98eab71be9ad788dcc1a397975e9f40a77485d16
Albert Ishmukhamedov <albert.ishmukhamedov@yandex.ru>
2019-02-03 16:51:26 +0500

    Task 04_student (lesson_003) is completed

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -10,5 +10,30 @@
 #   Студенту надо попросить ХХХ.ХХ рублей
 
 educational_grant, expenses = 10000, 12000
+i = 1
+total_expenses = 0
+parents_grant = 0
+school_year = 10
 
-# TODO здесь ваш код
+while i <= school_year:
+    total_expenses += expenses
+    expenses += expenses*.03
+    i += 1
+
+parents_grant = total_expenses - school_year * educational_grant
+print('Студенту надо попросить', round(parents_grant, 2), 'рублей')
+
+# другой вариант
+
+educational_grant, expenses = 10000, 12000
+i = 1
+total_expenses = 0
+parents_grant = 0
+school_year = 10
+
+while i <= school_year:
+    parents_grant += expenses - educational_grant
+    expenses += expenses*.03
+    i += 1
+
+print('Студенту надо попросить', round(parents_grant, 2), 'рублей')
