cbbe8c2c04e6d182b77fbfec3bacde13c0c3eef4
Albert Ishmukhamedov <albert.ishmukhamedov@yandex.ru>
2019-02-03 22:24:36 +0500

    Task 07_wall (lesson_003) is completed

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+# sd.resolution = (1200, 600)
+# sd.background_color = sd.random_color()
+# color = sd.random_color()
 
-simple_draw.pause()
+color = sd.COLOR_DARK_CYAN
+max_x, max_y = sd.resolution[0], sd.resolution[1]
+size_x, size_y = 100, 50
+
+for y in range(0, max_y, size_y):
+    for x in range(0, max_x, size_x):
+        delta = size_x*((y / size_y) % 2)/2
+        sd.line(sd.get_point(x - delta, y), sd.get_point(x - delta + size_x, y), color)
+        sd.line(sd.get_point(x - delta + size_x, y), sd.get_point(x - delta + size_x, y + size_y), color)
+        sd.line(sd.get_point(x - delta + size_x, y + size_y), sd.get_point(x - delta, y + size_y), color)
+        sd.line(sd.get_point(x - delta, y + size_y), sd.get_point(x - delta, y), color)
+
+sd.pause()
