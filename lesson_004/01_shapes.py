f2e1c5d73faa2695da87d75ea3293e95fe4ca055
Albert Ishmukhamedov <albert.ishmukhamedov@yandex.ru>
2019-02-09 18:37:58 +0500

    Task 01_shapes is completed.

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
+from math import sin, cos, pi
 
 # Написать функции рисования равносторонних геометрических фигур:
 # - треугольника
@@ -22,6 +23,41 @@ import simple_draw as sd
 # sd.line()
 # Результат решения см lesson_004/results/exercise_01_shapes.jpg
 
-# TODO здесь ваш код
+
+def polygon(start_point, angle, length, color=sd.COLOR_DARK_GREEN, width=2):
+    n = int(360/angle)
+    x, y = start_point.x, start_point.y
+
+    for i in range(n-1):
+        vector = sd.get_vector(start_point=start_point, angle=angle+i*angle, length=length, width=width)
+        vector.draw(color=color)
+        start_point = vector.end_point
+
+    sd.line(sd.get_point(x, y), start_point, color=color, width=width)
+
+
+def draw_polygon(center, angles=3, size=100):
+    """
+        Более математическая версия. Рисует многоугольник с центром в точке center, с количеством углов angles
+        и размером (радиус описанной вокруг многоугольника окружности) size.
+    """
+
+    points = list()
+    x, y = center.x, center.y
+
+    for i in range(angles):
+        x1 = x + size*cos(90+(2*pi*i)/angles)
+        y1 = y + size*sin(90+(2*pi*i)/angles)
+        points.append(sd.get_point(x1, y1))
+
+    sd.polygon(points, width=2)
+
+
+length = 100
+
+for i in [3, 4, 5, 6]:
+    point = sd.get_point((i-2)*100, (i-2)*100)
+    polygon(point, angle=360/i, length=length)
+    draw_polygon(point, i, length)
 
 sd.pause()
