c559e406b995c07ce328457b7af07b1243f5aec7
Albert Ishmukhamedov <albert.ishmukhamedov@yandex.ru>
2019-02-10 15:07:39 +0500

    Task 02_global_color is completed.

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -14,6 +14,39 @@ import simple_draw as sd
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
-# TODO здесь ваш код
+
+def polygon(start_point, angle, length, color=sd.COLOR_DARK_GREEN, width=2):
+    n = int(360/angle)
+    x, y = start_point.x, start_point.y
+
+    for i in range(n-1):
+        vector = sd.get_vector(start_point=start_point, angle=angle+i*angle, length=length, width=width)
+        vector.draw(color=color)
+        start_point = vector.end_point
+
+    sd.line(sd.get_point(x, y), start_point, color=color, width=width)
+
+
+colors = [['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple'], [sd.COLOR_RED, sd.COLOR_ORANGE,
+          sd.COLOR_YELLOW, sd.COLOR_GREEN, sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE]]
+
+points = [[200, 100], [500, 100], [200, 400], [500, 400]]
+
+print("Возможные цвета:")
+for num, color in enumerate(colors[0]):
+    print(num, ':', color)
+
+clr = int(input("Введите желаемый цвет > "))
+while clr not in range(7):
+    print("Вы ввели некоректный номер!")
+    clr = int(input("Введите желаемый цвет > "))
+
+clr = colors[1][clr]
+size = 100
+
+for i in [3, 4, 5, 6]:
+    point = sd.get_point(points[i-3][0], points[i-3][1])
+    polygon(point, angle=360/i, color=clr, length=size)
+
 
 sd.pause()
