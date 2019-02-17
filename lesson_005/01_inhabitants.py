06831297ffb38444d90c33b28a4a5a888e22e63d
Albert Ishmukhamedov <albert.ishmukhamedov@yandex.ru>
2019-02-17 16:29:04 +0500

    Task 01_inhabitants from lesson_005 is completed.

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from lesson_005 import room_1 as r1
+from lesson_005 import room_2 as r2
+
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
 # TODO здесь ваш код
+print('В комнате room_1 живут:')
+for i, person in enumerate(r1.folks):
+    print('\t', i + 1, ') ', person, sep='')
 
-
+print('В комнате room_2 живут:')
+for i, person in enumerate(r2.folks):
+    print('\t', i + 1, ') ', person, sep='')
