d6552a7ffc173e2f6d4191e8da20455e14882d58
Albert Ishmukhamedov <albert.ishmukhamedov@ynadex.ru>
2019-02-22 12:55:06 +0500

    Task 02_district from lesson_005 is fixed.

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -1,28 +1,29 @@
 # -*- coding: utf-8 -*-
-# TODO Можно изменить импорты на
-#  from ...house1.room1 import folks as dch1r1_folks
-#  И далее использоват напрямую без точки.
-from lesson_005.district.central_street.house1 import room1 as dch1r1, room2 as dch1r2
-from lesson_005.district.central_street.house2 import room1 as dch2r1, room2 as dch2r2
-from lesson_005.district.soviet_street.house1 import room1 as dsh1r1, room2 as dsh1r2
-from lesson_005.district.soviet_street.house2 import room1 as dsh2r1, room2 as dsh2r2
+
+from lesson_005.district.central_street.house1.room1 import folks as dch1r1_folks
+from lesson_005.district.central_street.house1.room2 import folks as dch1r2_folks
+from lesson_005.district.central_street.house2.room1 import folks as dch2r1_folks
+from lesson_005.district.central_street.house2.room2 import folks as dch2r2_folks
+from lesson_005.district.soviet_street.house1.room1 import folks as dsh1r1_folks
+from lesson_005.district.soviet_street.house1.room2 import folks as dsh1r2_folks
+from lesson_005.district.soviet_street.house2.room1 import folks as dsh2r1_folks
+from lesson_005.district.soviet_street.house2.room2 import folks as dsh2r2_folks
 
 # Составить список всех живущих на районе и Вывести на консоль через запятую
 # Формат вывода: На районе живут ...
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-
 people = []
 
-people.extend(dch1r1.folks)
-people.extend(dch1r2.folks)
-people.extend(dch2r1.folks)
-people.extend(dch2r2.folks)
-people.extend(dsh1r1.folks)
-people.extend(dsh1r2.folks)
-people.extend(dsh2r1.folks)
-people.extend(dsh2r2.folks)
-# TODO Можно задать people как сумму списков folks.
+people += dch1r1_folks
+people += dch1r2_folks
+people += dch2r1_folks
+people += dch2r2_folks
+people += dsh1r1_folks
+people += dsh1r2_folks
+people += dsh2r1_folks
+people += dsh2r2_folks
+
 inhabitants = ', '.join(people)
 print('На районе живут:', inhabitants)
