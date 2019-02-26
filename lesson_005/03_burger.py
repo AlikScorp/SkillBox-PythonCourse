b23d81a4ce3610278080655c597d321b054bf0b7
Albert Ishmukhamedov <albert.ishmukhamedov@ynadex.ru>
2019-02-26 12:41:51 +0500

    Task 03_burger from lesson_005 is fixed.

--- lesson_005/03_burger.py
+++ lesson_005/03_burger.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
-# TODO Импортировать все содержимое модуля плохая практика.
-#  Нужно либо импортировать модуль и ссылаться на него.
-#  from lesson_005 import my_burger
-#  my_burger.add_bun()
-#  Либо перечислять при импорте то, что будете использовать.
-#  from lesson_005.my_burger import (add_bun, add_cheese, ...)
-from lesson_005.my_burger import *
+import lesson_005.my_burger as mb
 # Создать модуль my_burger. В нем определить функции добавления инградиентов:
 #  - булочки
 #  - котлеты
@@ -22,27 +16,27 @@ from lesson_005.my_burger import *
 # Если не хватает инградиентов - создать соответствующие функции в модуле my_burger
 
 print('Давайте приготовим "Двойной Чизбургер":')
-add_bun()
-add_beef_patty()
-add_cheese()
-add_beef_patty(1)
-add_cheese(1)
-add_pickles()
-add_onion()
-add_ketchup()
-add_mustard()
-add_bun(1)
-is_ready()
+mb.add_bun()
+mb.add_beef_patty()
+mb.add_cheese()
+mb.add_beef_patty(1)
+mb.add_cheese(1)
+mb.add_pickles()
+mb.add_onion()
+mb.add_ketchup()
+mb.add_mustard()
+mb.add_bun(1)
+mb.is_ready()
 print('Но не забывайте - в нём 442Ккал!!! :-)')
-
+input()
 print('\nИ мой любимый рецепт - "Чикен Гурмэ Экзотик":')
-add_bun()
-add_mustard()
-add_chicken()
-add_tomato()
-add_salad()
-add_guacamole()
-add_bun(1)
-is_ready()
+mb.add_bun()
+mb.add_mustard()
+mb.add_chicken()
+mb.add_tomato()
+mb.add_salad()
+mb.add_guacamole()
+mb.add_bun(1)
+mb.is_ready()
 print('Но не забывайте -  в нём 621Ккал!!! :-)')
 
