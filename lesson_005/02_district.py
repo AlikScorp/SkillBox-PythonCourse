d4b43f87f6a8a780d4c2464011e7eaa579c5b2c9
Albert Ishmukhamedov <albert.ishmukhamedov@ynadex.ru>
2019-02-26 12:35:28 +0500

    Task 02_district from lesson_005 is fixed.

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -16,20 +16,7 @@ from lesson_005.district.soviet_street.house2.room2 import folks as dsh2r2_folks
 
 people = []
 
-people += dch1r1_folks
-people += dch1r2_folks
-people += dch2r1_folks
-people += dch2r2_folks
-people += dsh1r1_folks
-people += dsh1r2_folks
-people += dsh2r1_folks
-people += dsh2r2_folks
-# TODO Вместо добавления каждой комнаты по отдельности можно задать
-#  people как сумму *_folks одной операцией. Т. е. вмемто
-#  my_list = []
-#  my_list += [1, 2]
-#  my_list += [3, 4]
-#  сделать
-#  my_list = [1, 2] + [3, 4]
+people = dch1r1_folks + dch1r2_folks + dch2r1_folks + dch2r2_folks +\
+         dsh1r1_folks + dsh1r2_folks + dsh2r1_folks + dsh2r2_folks
 inhabitants = ', '.join(people)
 print('На районе живут:', inhabitants)
