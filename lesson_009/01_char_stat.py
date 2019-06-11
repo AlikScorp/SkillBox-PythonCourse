5e721e318708a3394789d0a53aaf3aea54c46040
Albert Ishmukhamedov <albert.ishmukhamedov@ynadex.ru>
2019-06-11 15:21:48 +0500

    Task 01_char_stat from lesson_009 is fixed.

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -83,11 +83,7 @@ class CharCounter:
 
         Обрабатывает случай получения в качестве аргумента не зазипованного текстового файла.
 
-        # Можно выводить отсортировано чрез метод display с помощью параметра sorting (по умолчанию sorting = 0)
-        # 0 - по алфавиту,
-        # 1 - по алвавиту (реверсивно),
-        # 2 - по количеству,
-        # 3 - по количеству (реверсивно)
+        Метод output выводит результаты подсчета (результаты не сортированы)
     """
 
     SORTED_BY_ALPHABET = 0
@@ -124,7 +120,7 @@ class CharCounter:
             else:
                 continue
 
-    def count(self):
+    def _count(self):
 
         if not self._find_file():
             return False
@@ -163,43 +159,137 @@ class CharCounter:
         cprint('\nВсе подсчитано.', color='cyan')
         return True
 
-    def display(self, sorting=0):
-        # Шапка таблицы
-        print('+' + '-'*10 + '+' + '-'*10 + '+')
+    @staticmethod
+    def _print_header():
+        print('+' + '-' * 10 + '+' + '-' * 10 + '+')
         print(f"|{'Буква':^10}|{'Частота':^10}|")
-        print('+' + '-'*10 + '+' + '-'*10 + '+')
+        print('+' + '-' * 10 + '+' + '-' * 10 + '+')
+
+    @staticmethod
+    def _print_footer():
+        print('+' + '-' * 10 + '+' + '-' * 10 + '+')
+
+    def output(self):
+
+        if self._count():
+            cprint('Выводим результаты подсчета:', color='yellow')
+        else:
+            cprint('Не найден входной файл!', color='red')
+            return
+
+        # Шапка таблицы
+        self._print_header()
+
+        for symbol in self.symbols:
+            print(f"|{symbol:^10}|{self.symbols[symbol]:>10}|")
+
+        # подвал таблицы
+        self._print_footer()
+
+
+class CharCounterSortedAlphabet(CharCounter):
+    """
+        Класс переопределяет метод output из класса CharCounter
+        Метод output выводит результаты подсчета отсортированые по алфавиту (по возрастанию)
+    """
+
+    def output(self):
+
+        if self._count():
+            cprint('Выводим результаты - отсортированно по алфавиту(по возрастанию):', color='yellow')
+        else:
+            cprint('Не найден входной файл!', color='red')
+            return
+
+        self._print_header()
 
         keys = list(self.symbols.keys())    # Создаем лист из ключей словаря self.symbol
+
+        keys.sort()  # Сортируем лист по возрастанию
+        for key in keys:
+            print(f"|{key:^10}|{self.symbols[key]:>10}|")
+
+        self._print_footer()
+
+
+class CharCounterSortedAlphabetReverse(CharCounter):
+    """
+        Класс переопределяет метод output из класса CharCounter
+        Метод output выводит результаты подсчета отсортированые по алфавиту (по убыванию)
+    """
+
+    def output(self):
+
+        if self._count():
+            cprint('Выводим результаты - отсортированно по алфавиту(по убыванию):', color='yellow')
+        else:
+            cprint('Не найден входной файл!', color='red')
+            return
+
+        self._print_header()
+
+        keys = list(self.symbols.keys())    # Создаем лист из ключей словаря self.symbol
+
+        keys.sort(reverse=True)  # Сортируем лист по убыванию
+        for key in keys:
+            print(f"|{key:^10}|{self.symbols[key]:>10}|")
+
+        self._print_footer()
+
+
+class CharCounterSortedQuantity(CharCounter):
+    """
+        Класс переопределяет метод output из класса CharCounter
+        Метод output выводит результаты подсчета отсортированые по частоте использования (по возрастанию)
+    """
+
+    def output(self):
+
+        if self._count():
+            cprint('Выводим результаты - отсортированно по частоте использования (по возрастанию):', color='yellow')
+        else:
+            cprint('Не найден входной файл!', color='red')
+            return
+
+        self._print_header()
+
         items = list(self.symbols.items())  # Создаем лист кортежей типа (key, value) из словаря self.symbol
 
-        if sorting == self.SORTED_BY_ALPHABET:
-            keys.sort()  # Сортируем лист по возрастанию
-            for key in keys:
-                print(f"|{key:^10}|{self.symbols[key]:>10}|")
-        elif sorting == self.SORTED_BY_ALPHABET_REVERSE:
-            keys.sort(reverse=True)  # Сортируем лист по убыванию
-            for key in keys:
-                print(f"|{key:^10}|{self.symbols[key]:>10}|")
-        elif sorting == self.SORTED_BY_QUANTITY:
-            items.sort(key=operator.itemgetter(1))  # Сортируем лист (по возрастанию) по второму элементу кортежа
-            for key, value in items:
-                print(f"|{key:^10}|{value:>10}|")
-        elif sorting == self.SORTED_BY_QUANTITY_REVERSE:
-            items.sort(key=operator.itemgetter(1), reverse=True)  # Сортируем лист (по убыванию)
-            for key, value in items:
-                print(f"|{key:^10}|{value:>10}|")
-        else:
-            pass
+        items.sort(key=operator.itemgetter(1))  # Сортируем лист (по возрастанию) по второму элементу кортежа
+        for key, value in items:
+            print(f"|{key:^10}|{value:>10}|")
 
-        print('+' + '-'*10 + '+' + '-'*10 + '+')
+        self._print_footer()
+
+
+class CharCounterSortedQuantityReverse(CharCounter):
+    """
+        Класс переопределяет метод output из класса CharCounter
+        Метод output выводит результаты подсчета отсортированые по частоте использования (по убыванию)
+    """
+
+    def output(self):
+
+        if self._count():
+            cprint('Выводим результаты - отсортированно по частоте использования (по убыванию):', color='yellow')
+        else:
+            cprint('Не найден входной файл!', color='red')
+            return
+
+        self._print_header()
+
+        items = list(self.symbols.items())  # Создаем лист кортежей типа (key, value) из словаря self.symbol
+
+        items.sort(key=operator.itemgetter(1), reverse=True)  # Сортируем лист (по убыванию) по второму элементу кортежа
+        for key, value in items:
+            print(f"|{key:^10}|{value:>10}|")
+
+        self._print_footer()
 
 
 if __name__ == '__main__':
-    counter = CharCounter('voyna-i-mir.txt.zip')
-    if counter.count():
-        counter.display()
-    else:
-        cprint('Не найден входной файл', color='red')
+    counter = CharCounterSortedQuantityReverse('voyna-i-mir.txt.zip')
+    counter.output()
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
