3db9e84897f2dce4d7f4defd5991300a8e89b065
Albert Ishmukhamedov <albert.ishmukhamedov@yandex.ru>
2019-07-26 00:07:13 +0300

    Task 01_fifth_element from lesson_010 is done.

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -5,9 +5,18 @@
 BRUCE_WILLIS = 42
 
 input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+
+try:
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+except ValueError as exc:
+    print(f'В веденной строке под индексом четыре не число. Ошибка - "{exc}" с параметрами {exc.args}!')
+except IndexError as exc:
+    print(f'В веденной строке нет индекса с номером 4. Ошибка - "{exc}" с параметрами {exc.args}!')
+except Exception as exc:
+    print(f'Возникла неизвестная ошибка {exc} с параметрами {exc.args}!')
+else:
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
