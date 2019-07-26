f2216c8ef162bf3f7a9d1e51facd1034a330b310
Albert Ishmukhamedov <albert.ishmukhamedov@yandex.ru>
2019-07-26 09:36:38 +0300

    Task 02_groundhog_day from lesson_010 is done.

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -17,8 +17,75 @@
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
 
+from random import randint
+
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
-# TODO здесь ваш код
+
+class GroundhogDayError(Exception):
+    pass
+
+
+class IamGodError(GroundhogDayError):
+    pass
+
+
+class DrunkError(GroundhogDayError):
+    pass
+
+
+class CarCrushError(GroundhogDayError):
+    pass
+
+
+class GluttonyError(GroundhogDayError):
+    pass
+
+
+class DepressionError(GroundhogDayError):
+    pass
+
+
+class SuicideError(GroundhogDayError):
+    pass
+
+
+def one_day():
+    karma = randint(1, 13)
+
+    if karma <= 7:
+        return karma
+    elif karma == 8:
+        raise IamGodError("I'm a god. I'm not *the* God... I don't think.")
+    elif karma == 9:
+        raise DrunkError("Drunk is more fun.")
+    elif karma == 10:
+        raise CarCrushError("I'm betting he's going to swerve first.")
+    elif karma == 11:
+        raise GluttonyError("Would you like to come to dinner with Larry and me?")
+    elif karma == 12:
+        raise DepressionError("Well, what if there is no tomorrow? There wasn't one today.")
+    else:
+        raise SuicideError("I've been stabbed, shocked, poisoned, frozen, hung, electrocuted, and burned.")
+
+
+total_karma = 0
+f = None
+
+try:
+    f = open('groundhog_day.log', 'w')
+except OSError as exc:
+    print(f"The file cannot be opened: {exc}")
+else:
+    while total_karma < ENLIGHTENMENT_CARMA_LEVEL:
+        try:
+            total_karma += one_day()
+        except GroundhogDayError as exc:
+            f.write(f"{exc.__class__} -> {exc}\n")
+finally:
+    if f is not None:
+        f.close()
+
+print(f'The total karma is: {total_karma}')
 
 # https://goo.gl/JnsDqu
