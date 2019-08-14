e5c5462fb9a14a93e7407e1a953006489762df56
Albert Ishmukhamedov <albert.ishmukhamedov@yandex.ru>
2019-08-14 23:41:43 +0500

    Task 04_error_log_decorator from lesson_011 is done.

--- lesson_011/04_error_log_decorator.py
+++ lesson_011/04_error_log_decorator.py
@@ -6,26 +6,52 @@
 # Имя файла лога - function_errors.log
 # Формат лога: <имя функции> <параметры вызова> <тип ошибки> <текст ошибки>
 # Лог файл открывать каждый раз при ошибке в режиме 'a'
+from termcolor import cprint
 
 
-def log_errors(func):
-    pass
-    # TODO здесь ваш код
+def log_errors(filename: str):
+    """
+        Функция создает декоратор. Создаваемый декоратор перехватывает иключения,
+        возникающие в декорируемой функции и записывает их в файл,
+        имя которого получает в качестве параметра данная функция.
+    :param filename: Имя создаваемого файла
+    :return: Функцию-декоратор
+    """
 
+    file = None
+
+    try:
+        file = open(filename, 'a', encoding='utf8')
+    except IOError:
+        cprint(f'Cannot open the file "{filename}"')
+        raise SystemExit
+    else:
+        def log_decorator(func):
+            def surrogate(*args, **kwargs):
+                try:
+                    result = func(*args, **kwargs)
+                except (ValueError, ZeroDivisionError) as exc:
+                    file.write(f'The function "{func.__name__}" with parameters: {args}, {kwargs} '
+                               f'has raised the following exception: {exc}\n')
+                else:
+                    return result
+            return surrogate
+
+        return log_decorator
 
 # Проверить работу на следующих функциях
-@log_errors
+@log_errors(filename='function_errors.log')
 def perky(param):
     return param / 0
 
 
-@log_errors
-def check_line(line):
-    name, email, age = line.split(' ')
+@log_errors(filename='function_errors.log')
+def check_line(str_line):
+    name, email, age = str_line.split(' ')
     if not name.isalpha():
         raise ValueError("it's not a name")
     if '@' not in email or '.' not in email:
-        raise ValueError("it's not a email")
+        raise ValueError("it's not an email")
     if not 10 <= int(age) <= 99:
         raise ValueError('Age not in 10..99 range')
 
